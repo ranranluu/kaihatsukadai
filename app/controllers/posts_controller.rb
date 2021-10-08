a85fd88234d6e96e50cced86ecf4854b6126646b
ranranluu <ranranluutennis@gmail.com>
2021-10-08 06:23:13 +0000

    [fix]部分テンプレートとレスポンシブ対応

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
+      flash[:notice]="You have posted dairy successfully."
       redirect_to posts_path
     else
       render :new
@@ -27,6 +28,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.user_id = current_user.id
     if @post.update(post_params)
+      flash[:notice]="You have updated dairy successfully."
       redirect_to posts_path
     else
       render :edit
@@ -37,6 +39,7 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     post.user_id = current_user.id
     post.destroy
+    flash[:notice]="You have deleted dairy successfully."
     redirect_to posts_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice]="You have updated your information successfully."
       redirect_to user_path(current_user)
     else
       render :edit
